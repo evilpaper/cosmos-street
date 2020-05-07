a40a2054e253fd7776e95f5a31566b4d0f5f92b1
evilpaper <pellburt@gmail.com>
2020-05-07 14:41:58 +0200

    Move player code into player object

--- game.js
+++ game.js
@@ -6,90 +6,94 @@ const context = canvas.getContext("2d");
 
 const gravity = -1;
 
-const player = new Image();
-player.src = "spritesheet.png";
-player.tick = 0;
-player.frame = 0;
-player.ticksToNextFrame = 16;
-player.yy = 20;
-player.speed = 1;
-player.states = ["skating", "jumping"];
-player.state = player.states[0];
+const p = {
+  name: "Player 1",
+  image: new Image(),
+  tick: 0,
+  frame: 0,
+  ticksToNextFrame: 16,
+  yy: 20,
+  speed: 1,
+  states: ["skating", "jumping"],
+  state: "skating",
+};
+
+p.image.src = "spritesheet.png";
 
 const tile = new Image();
 tile.src = "spritesheet.png";
 tile.tick = 0;
 
 function update() {
-  if (player.yy <= 39) {
-    player.yy = player.yy - gravity;
+  if (p.yy <= 39) {
+    p.yy = p.yy - gravity;
   }
 
-  if (player.yy === 39) {
-    player.state = player.states[0];
+  if (p.yy === 39) {
+    p.state = p.states[0];
   }
 
-  player.tick = (player.tick + 1) % player.ticksToNextFrame; // 1, 0, 1, 0 etc...
+  p.tick = (p.tick + 1) % p.ticksToNextFrame; // 1, 0, 1, 0 etc...
 
-  if (player.tick === 0) {
-    player.frame = player.frame + 1;
-    if (player.frame >= 2) {
-      player.frame = 0;
+  if (p.tick === 0) {
+    p.frame = p.frame + 1;
+    if (p.frame >= 2) {
+      p.frame = 0;
     }
   }
 
-  tile.tick = (tile.tick + player.speed) % 16;
+  tile.tick = (tile.tick + p.speed) % 16;
 }
 
 function draw() {
   context.clearRect(0, 0, 256, 256);
-  context.drawImage(tile, 0, 35, 16, 16, 131 - tile.tick, 75, 16, 16);
-  context.drawImage(tile, 0, 35, 16, 16, 115 - tile.tick, 75, 16, 16);
-  context.drawImage(tile, 0, 35, 16, 16, 99 - tile.tick, 75, 16, 16);
-  context.drawImage(tile, 0, 35, 16, 16, 83 - tile.tick, 75, 16, 16);
-  context.drawImage(tile, 0, 35, 16, 16, 67 - tile.tick, 75, 16, 16);
-  context.drawImage(tile, 0, 35, 16, 16, 51 - tile.tick, 75, 16, 16);
-  context.drawImage(tile, 0, 35, 16, 16, 35 - tile.tick, 75, 16, 16);
-  context.drawImage(tile, 0, 35, 16, 16, 19 - tile.tick, 75, 16, 16);
-  context.drawImage(tile, 0, 35, 16, 16, 3 - tile.tick, 75, 16, 16);
+  context.drawImage(tile, 0, 35, 16, 16, 140 - tile.tick, 75, 16, 16);
+  context.drawImage(tile, 0, 35, 16, 16, 123 - tile.tick, 75, 16, 16);
+  context.drawImage(tile, 0, 35, 16, 16, 106 - tile.tick, 75, 16, 16);
+  context.drawImage(tile, 0, 35, 16, 16, 89 - tile.tick, 75, 16, 16);
+  context.drawImage(tile, 0, 35, 16, 16, 72 - tile.tick, 75, 16, 16);
+  context.drawImage(tile, 0, 35, 16, 16, 55 - tile.tick, 75, 16, 16);
+  context.drawImage(tile, 0, 35, 16, 16, 38 - tile.tick, 75, 16, 16);
+  context.drawImage(tile, 0, 35, 16, 16, 21 - tile.tick, 75, 16, 16);
+  context.drawImage(tile, 0, 35, 16, 16, 4 - tile.tick, 75, 16, 16);
   context.drawImage(tile, 0, 35, 16, 16, -13 - tile.tick, 75, 16, 16);
-  if (player.state === "skating") {
-    if (player.frame === 0) {
-      context.drawImage(player, 0, 0, 26, 35, 51, player.yy, 26, 35);
+  if (p.state === "skating") {
+    if (p.frame === 0) {
+      context.drawImage(p.image, 0, 0, 26, 35, 51, p.yy, 26, 35);
     } else {
-      context.drawImage(player, 26, 0, 26, 35, 51, player.yy, 26, 35);
+      context.drawImage(p.image, 26, 0, 26, 35, 51, p.yy, 26, 35);
     }
   }
-  if (player.state === "jumping") {
-    context.drawImage(player, 52, 0, 26, 40, 51, player.yy - 3, 26, 40);
+  if (p.state === "jumping") {
+    context.drawImage(p.image, 52, 0, 26, 40, 51, p.yy - 3, 26, 40);
   }
 }
 
 document.addEventListener("keyup", (event) => {
   if (event.keyCode === 39) {
-    player.speed = 1;
+    p.speed = 1;
   }
   if (event.keyCode === 37) {
-    player.speed = 1;
-    player.state = player.states[0];
+    p.speed = 1;
+    p.state = p.states[0];
   }
 });
 
 document.addEventListener("keydown", (event) => {
   if (event.keyCode === 38) {
-    player.yy = player.yy - 40;
-    player.state = player.states[1];
+    p.yy = p.yy - 40;
+    p.state = p.states[1];
   }
   if (event.keyCode === 40) {
-    player.yy = player.yy + 10;
-    player.state = player.states[0];
+    p.yy = p.yy + 10;
+    p.state = p.states[0];
   }
   if (event.keyCode === 39) {
-    player.speed = 2;
+    p.speed = 2;
   }
   if (event.keyCode === 37) {
-    player.speed = 0.5;
-    player.state = player.states[1];
+    p.speed = 0.5;
+    p.state = p.states[1];
   }
 });
 
