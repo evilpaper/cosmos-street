4280ad77ac2b60e4fe7dff8f41de05db74005a89
evilpaper <pellburt@gmail.com>
2020-06-01 16:56:31 +0200

    Add twinkling star

--- game.js
+++ game.js
@@ -3,9 +3,13 @@
 
 // TODO
 
+// Remove p.x change on speed up
+// Implement landscape mode
+// Implement portrait mode (rotate or get a real screen)
+// Touch controlls
+// Continues integration
 // Collision
-// Physics
-// Animations
+// Game States (start, game, end)
 // Levels
 // Life
 // Power-ups
@@ -41,13 +45,12 @@ class Player {
     this.name = "Player 1";
     this.image = new Image();
     this.image.src = "spritesheet.png";
+    this.ticksToNextFrame = 16;
     this.tick = 0;
     this.frame = 0;
     this.x = 51;
     this.y = 20;
     this.dy = 0;
-    this.dx = 0;
-    this.ticksToNextFrame = 16;
     this.speed = 2;
     this.states = ["skating", "jumping", "breaking"];
     this.state = this.states[1];
@@ -64,36 +67,33 @@ class Player {
       }
       if (controller.right) {
         p.speed = 3;
-        p.state = p.states[0];
       }
     }
     if (p.state === "jumping") {
       p.dy += gravity;
-      p.y = Math.round(p.y + p.dy);
       p.dy += friction;
+      p.y = p.y + p.dy;
     }
     if (p.state === "breaking") {
       p.speed = 1;
       if (!controller.left) {
         p.state = p.states[0];
       }
+      if (controller.up) {
+        p.dy = -6;
+        p.state = p.states[1];
+      }
     }
 
-    // if (!controller.left && p.state === "breaking") {
-    //   p.speed = 2;
-    //   p.state = p.states[0];
-    // }
-    // if (!controller.right && p.speed === 3) {
-    //   p.speed = 2;
-    // }
-
-    p.tick = (p.tick + 1) % p.ticksToNextFrame; // 1, 0, 1, 0 etc...
-
+    // Collision
     if (p.y > 64) {
       p.state = p.states[0];
       p.dy = 0;
       p.y = 64;
     }
+
+    // Animation
+    p.tick = (p.tick + 1) % p.ticksToNextFrame; // 1, 0, 1, 0 etc...
 
     if (p.tick === 0) {
       p.frame = p.frame + 1;
@@ -111,18 +111,37 @@ function Tile() {
   this.tick = 0;
 }
 
-const p = new Player();
+function Star() {
+  this.name = "star";
+  this.image = new Image();
+  this.image.src = "star-1.png";
+  this.tick = 0;
+  this.frame = 0;
+  this.ticksToNextFrame = 16;
+  this.update = function () {
+    this.tick = (this.tick + 1) % this.ticksToNextFrame; // 1, 0, 1, 0 etc...
 
+    if (this.tick === 0) {
+      this.frame = this.frame + 1;
+      if (this.frame >= 6) {
+        this.frame = 0;
+      }
+    }
+  };
+}
+
+const p = new Player();
 const tile = new Tile();
+const star1 = new Star();
 
 function update() {
   p.update();
-
+  star1.update();
   tile.tick = Math.round((tile.tick + p.speed) % 34);
 }
 
 function draw() {
-  console.log(p.state);
+  // console.log(p.state);
   context.clearRect(0, 0, 256, 256);
   context.drawImage(tile.image, 0, 35, 16, 16, -17 - tile.tick, 100, 16, 16);
   context.drawImage(tile.image, 0, 35, 16, 16, 0 - tile.tick, 100, 16, 16);
@@ -140,16 +159,47 @@ function draw() {
   context.drawImage(tile.image, 0, 35, 16, 16, 187 - tile.tick, 100, 16, 16);
   context.drawImage(tile.image, 0, 35, 16, 16, 204 - tile.tick, 100, 16, 16);
   context.drawImage(tile.image, 0, 35, 16, 16, 221 - tile.tick, 100, 16, 16);
+  context.drawImage(star1.image, 0 + star1.frame * 7, 0, 7, 7, 100, 34, 7, 7);
 
-  if (p.state === "skating") {
+  if (p.state === "skating" || p.state === "speeding") {
     if (p.frame === 0) {
-      context.drawImage(p.image, 0, 0, 26, 35, p.x, p.y, 26, 35);
+      context.drawImage(
+        p.image,
+        0,
+        0,
+        26,
+        35,
+        Math.round(p.x),
+        Math.round(p.y),
+        26,
+        35
+      );
     } else {
-      context.drawImage(p.image, 26, 0, 26, 35, p.x, p.y, 26, 35);
+      context.drawImage(
+        p.image,
+        26,
+        0,
+        26,
+        35,
+        Math.round(p.x),
+        Math.round(p.y),
+        26,
+        35
+      );
     }
   }
   if (p.state === "jumping" || p.state === "breaking") {
-    context.drawImage(p.image, 52, 0, 26, 40, p.x, p.y - 3, 26, 40);
+    context.drawImage(
+      p.image,
+      52,
+      0,
+      26,
+      40,
+      Math.round(p.x),
+      Math.round(p.y - 3),
+      26,
+      40
+    );
   }
 }
 
